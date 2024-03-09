917da49ca69c04bd0949ffb8f45be53649279e9e
Gustavo Goulart <129804650+gustavo-goulart@users.noreply.github.com>
2024-03-09 16:07:34 -0300

    projeto calculadora

---
+++ projeto_calculadora/src/js/script.js
@@ -0,0 +1,30 @@
+function adicionarCaracter(caracter) {
+  const valorDisplay = document.querySelector(".display").value;
+
+  document.querySelector(".display").value = valorDisplay + caracter;
+}
+
+function limpaTela() {
+  document.querySelector(".display").value = "";
+}
+
+function calcular() {
+  const valorDisplay = document.querySelector(".display").value;
+
+  if (valorDisplay) {
+    // verifica se a expressão contém uma divisão por zero
+    if (valorDisplay.includes("/0")) {
+      alert("Não é possível fazer divisão por zero!");
+      clean();
+    } else {
+      // realiza a operação
+      document.querySelector(".display").value = eval(valorDisplay); //calcula
+    }
+  }
+}
+
+function inverterNumero() {
+  const valorDisplay = document.querySelector(".display").value;
+
+  document.querySelector(".display").value = valorDisplay * -1;
+}
