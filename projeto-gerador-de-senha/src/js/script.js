8e0db659ca320c6aa70f283ab96798bb1a5e722a
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-03-15 11:46:32 -0300

    configurando arquivos JavaSript
    estrutura JS completa

--- projeto-gerador-de-senha/src/js/script.js
+++ projeto-gerador-de-senha/src/js/script.js
@@ -1,15 +1,31 @@
+const inputElement = document.querySelector("#password");
+let passwordLength = 16;
+
 function generatePassword() {
   const chars =
     "abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]";
 
   let password = "";
 
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < passwordLength; i++) {
     const randomNumber = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNumber, randomNumber + 1);
 
-    console.log(password);
+    inputElement.value = password;
   }
 }
+
+function copyPassword() {
+  navigator.clipboard.writeText(inputElement.value);
+}
+
+const passwordLengthElement = document.querySelector("#password-length");
+passwordLengthElement.addEventListener("input", function () {
+  passwordLength = passwordLengthElement.value;
+  generatePassword();
+});
+
+const copyButtonElement = document.querySelector("#copy-password");
+copyButtonElement.addEventListener("click", copyPassword);
 
 generatePassword();
